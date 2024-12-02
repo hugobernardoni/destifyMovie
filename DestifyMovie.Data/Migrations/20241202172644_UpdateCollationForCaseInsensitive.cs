3507d107240e97abcbd7ce1c63cf92d6ab537461
hugo.oliveira <hugo.bernardoni@gmail.com>
2024-12-02 18:41:06 -0300

    Update migrations

---
+++ DestifyMovie.Data/Migrations/20241202172644_UpdateCollationForCaseInsensitive.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DestifyMovie.Data.Migrations
+{
+    public partial class UpdateCollationForCaseInsensitive : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Alterando o collation da coluna Title na tabela Movies
+            migrationBuilder.Sql(
+                "ALTER TABLE \"Movies\" ALTER COLUMN \"Title\" TYPE VARCHAR(255) COLLATE \"C\""
+            );
+
+            // Alterando o collation da coluna Name na tabela Actors
+            migrationBuilder.Sql(
+                "ALTER TABLE \"Actors\" ALTER COLUMN \"Name\" TYPE VARCHAR(255) COLLATE \"C\""
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Revertendo as alterações caso a migration seja revertida
+            migrationBuilder.Sql(
+                "ALTER TABLE \"Movies\" ALTER COLUMN \"Title\" TYPE VARCHAR(255)"
+            );
+
+            migrationBuilder.Sql(
+                "ALTER TABLE \"Actors\" ALTER COLUMN \"Name\" TYPE VARCHAR(255)"
+            );
+        }
+    }
+}
